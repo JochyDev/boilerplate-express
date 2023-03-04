e5df5680240d1bb09a3198955966f30a335fc9e0
JochyDev <joseluisjimenez1998@gmail.com>
2023-03-04 15:39:53 -0500

    finished 2

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ let app = express();
 require('dotenv').config();
 let bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(bodyParser.json())
 
 app.use(function(req, res, next){
