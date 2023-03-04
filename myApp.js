7e736e6e053c6372f087a7421399ca9fb96586d1
JochyDev <joseluisjimenez1998@gmail.com>
2023-03-04 13:13:06 -0500

    path now added

--- myApp.js
+++ myApp.js
@@ -26,6 +26,13 @@ app.get('/json', function(req, res){
     res.json({message})
 })
 
+app.get('/now', function(req, res, next){
+    req.time = new Date().toString();
+    next()
+}, function(req, res){
+    res.json({time: req.time})
+})
+
 
 
 
